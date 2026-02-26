3a8efcb6adc302f1d6230776b262d5cf2633d249
Ángelnm-nm <angel.martinnavas2323@gmail.com>
2026-02-26 02:40:23 +0100

    Add excercise 5b Create a use case that allows me to save albums.

--- src/main/java/edu/iesam/features/albums/domain/AlbumRepository.java
+++ src/main/java/edu/iesam/features/albums/domain/AlbumRepository.java
@@ -1,9 +1,12 @@
 package edu.iesam.features.albums.domain;
 
+import edu.iesam.features.songs.domain.Song;
+
 import java.util.ArrayList;
 
 public interface AlbumRepository {
 
     public ArrayList<Album> getAlbums();
+    public void saveAlbum(Album album);
 
 }
